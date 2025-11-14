b7b018e39bf76da12ec07ecfd045ab226898a3b1
Volney Powlis <realvolney@gmail.com>
2025-11-13 23:22:37 -0600

    updated run.cmd

--- run.cmd
+++ run.cmd
@@ -1,9 +1,5 @@
 @echo off
 setlocal
-
-REM Absolute path to Windows PowerShell (present on all supported Windows)
 set "PS=%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe"
-
-REM Resolve repo root and run the driver
 "%PS%" -NoLogo -NoProfile -ExecutionPolicy Bypass -File "%~dp0run.ps1" %*
 exit /b %ERRORLEVEL%
